77706c9ac708e55487f2474c6a509461bcd70db5
abcb2 <kouji.wtnb@gmail.com>
2018-02-22 12:47:40 +0900

    fix

--- chapter04/test/test_4-7_2.rb
+++ chapter04/test/test_4-7_2.rb
@@ -10,20 +10,18 @@ lambda do
     setups << block
   end
   Kernel.send :define_method, :event do |description, &block|
-    events << { description: description, block: block }
+    events << { description: description, condition: block }
   end
   Kernel.send :define_method, :each_event do |&block|
     events.each do |event|
       block.call event
     end
   end
-  Kernel.send :define_method, :each_setup do
+  Kernel.send :define_method, :each_setup do |&block|
     setups.each do |setup|
       block.call setup
     end
   end
-
-
 end.call
 
 setup "空の高さを設定" do
@@ -46,7 +44,7 @@ each_event do |event|
   each_setup do |setup|
     setup.call
   end
-  puts "ALERT: #{event[:description]}" if yield event[:conditon].call
+  puts "ALERT: #{event[:description]}" if event[:condition].call
 end
 
 class TestSample < Minitest::Test
