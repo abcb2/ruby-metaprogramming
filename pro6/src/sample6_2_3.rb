a7e9e640e54345d9d7d0d7564fe7f87dba5c7a79
abcb2 <kouji.wtnb@gmail.com>
2018-05-07 15:08:02 +0900

    mv

--- pro6/src/sample6_2_3.rb
+++ pro6/src/sample6_2_3.rb
@@ -1,83 +1,26 @@
-class Klass
-  @class_instance_val = :class_instance_val
-  @@class_val = :class_val
+module My
+  class Klass
+    @@count = 0
 
-  CONST_HOGE = "HOGE"
 
-  def method1
-    puts "A1: #{@class_instance_val}" # nil
-    puts "A2: #{@@class_val}"
-  end
+    def initialize
+      @@count += 1
+    end
 
-  def method2
-    puts "A3: #{CONST_HOGE}"
-  end
+    def method1
+      @@count
+    end
 
-  def method3
-    puts "A4: #{@@class_val}"
-  end
-
-  def method4
-    puts "A5: #{self.class.instance_variable_get(:@class_instance_val)}"
-    puts "A6: #{self}"
+    def self.check
+      @@count
+    end
   end
 end
 
-class Klass2 < Klass
-  @class_instance_val = :hogehoge
-
-  # def method4
-  #   puts "A7: #{self.class.instance_variable_get(:@class_instance_val)}"
-  #   puts "A8: #{self}"
-  # end
-
-  def method5
-    puts "A9: #{self.class.instance_variable_get(:@class_instance_val)}"
-    puts "A10: #{self}"
-  end
-
-  def method6
-    puts "A11: call method3"
-    method3
-  end
+k = My::Klass.new
+puts k.method1 # 1
+5.times do
+  My::Klass.new
 end
 
-k = Klass.new
-k.method1
-k.method2
-puts "B1: #{Klass::CONST_HOGE}"
-@@class_val = "OOPS" # Klass::CONST_HOGEを書き換えてしまう。(warningが出る)
-k.method3
-k.method4
-
-k2 = Klass2.new
-puts "-------->"
-k2.method4
-puts "-------->>"
-k2.method5
-k2.method6 # 書き換えたOOPSが出力される
-
-puts "==============================="
-
-class MyKlass
-  @class_instance_val = :hoge
-
-  def method1
-    puts "C1: #{self}"
-    puts "C2: #{self.class.instance_variable_get(:@class_instance_val)}"
-    self.class.instance_variable_get(:@class_instance_val)
-  end
-end
-
-class MyKlass2 < MyKlass
-  @class_instance_val = :moge
-end
-
-class MyKlass3 < MyKlass
-
-end
-
-myklass_obj2 = MyKlass2.new
-myklass_obj2.method1 # moge
-myklass_obj3 = MyKlass3.new
-puts myklass_obj3.method1().nil? # true
+puts My::Klass.check # 6
